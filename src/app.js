b71dae3c06c067d2cdfec59d4f781a0eabdcf98e
Aleksandar Mitrovic <alex@superplane.com>
2026-02-27 21:24:44 +0100

    Adding new page to test the workflow

--- src/app.js
+++ src/app.js
@@ -33,6 +33,10 @@ app.get('/products', (req, res) => {
   res.render('products/index', { products });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 app.get('/products/new', (req, res) => {
   res.render('products/new', { product: { name: '' }, errors: [] });
 });
