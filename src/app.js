6c0386082f643c86fdcc92444344d8a6c90f2919
Aleksandar Mitrovic <alex@superplane.com>
2026-03-01 18:51:55 +0100

    Intentionally break page

--- src/app.js
+++ src/app.js
@@ -34,7 +34,7 @@ app.get('/products', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-  res.render('about');
+  res.status(500).send('Intentional test failure for Dash0 alert validation.');
 });
 
 app.get('/products/new', (req, res) => {
